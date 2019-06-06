23848f4f5bb766d1ecb70187768f88b571eaf165
Rob Huls <rob.huls@nlr.nl>
2019-06-06 09:26:36 +0200

    First working sample

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 # Setup the server
 apt update
-apt-upgrade
+apt upgrade -y
 apt install python3-venv
 git clone https://github.com/robhuls/clubrecords.git
-cd clubrecords
+
+# Create a virtual environment for the app
+cd /clubrecords
 python3 -m venv clubrecords-env
-source ./clubrecords-env/bin/activate
+source /clubrecords-env/bin/activate
+pip install -r requirements.txt
+
+
